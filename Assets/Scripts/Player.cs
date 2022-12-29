7908b036c31ce8dbf05c8b0de227c5e227045378
Melody <reticivis@gmail.com>
2022-12-28 20:23:03 -0600

    the foundations

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2d;
+    private float _horizontal; 
+    private float _vertical;
+    public float speed = 3.0f;
+    private bool _grounded = false;
+    
+    private void Start()
+    {
+        _rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
+    }
+
+    
+    private void FixedUpdate()
+    {
+        Vector2 position = _rigidbody2d.position;
+        position.x += speed * _horizontal * Time.deltaTime;
+        position.y += speed * _vertical * Time.deltaTime;
+
+        _rigidbody2d.MovePosition(position);
+    }
+}
