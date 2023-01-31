37cee510b97e718d442a5f5f30b74174e3728159
Melody <reticivis@gmail.com>
2023-01-31 10:10:16 -0600

    not gate

--- Assets/Scripts/Gates/NotGate.cs
+++ Assets/Scripts/Gates/NotGate.cs
@@ -4,7 +4,12 @@ public class NotGate : MonoBehaviour, IGate
 {
     public ConnectionPoint IN;
     public ConnectionPoint OUT;
-
+    
+    void Start()
+    {
+        IN.outputs.Add(this);
+        UpdateState();
+    }
     public void UpdateState()
     {
         OUT.SetState(!IN.on);
