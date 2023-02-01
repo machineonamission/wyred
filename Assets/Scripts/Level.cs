e4b54d178cbe7a7febd05a138ff6ce169f9decc6
Melody <reticivis@gmail.com>
2023-02-01 12:36:34 -0600

    better text and level tester

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -1,5 +1,5 @@
-﻿
-using System;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -7,21 +7,70 @@ using UnityEngine;
 
 public class Level : MonoBehaviour
 {
-    [Serializable] public struct truthEntry
+    [Serializable]
+    public struct truthEntry
     {
         public List<bool> input;
         public List<bool> expectedOutput;
     }
+
+    public String levelName;
+    public int levelNumber;
     public List<ConnectionPoint> inputs = new();
     public List<ConnectionPoint> outputs = new();
     public List<truthEntry> truthTable = new();
     public GameObject textGameObject;
     private TextMeshProUGUI text;
+    private float _textTimeout;
+    public float updateSpeed = -1;
+    private int _cycleState = 0;
 
+    private int TwoToThe(int the)
+    {
+        int res = 1;
+        for (int i = 0; i < the; i++)
+        {
+            res *= 2;
+        }
+
+        return res;
+    }
     private void Start()
     {
         text = textGameObject.GetComponent<TextMeshProUGUI>();
+        text.text = $"Level {levelNumber}: {levelName}";
         Debug.Log(text);
+        _textTimeout = 5f;
+        if (updateSpeed > 0)
+        {
+            InvokeRepeating("UpdateInputs", updateSpeed, updateSpeed);
+        }
+    }
+
+    private void UpdateInputs()
+    {
+        // cycle through all possible iterations of the inputs via binary
+        _cycleState++;
+        if (_cycleState >= TwoToThe(inputs.Count))
+        {
+            _cycleState = 0;
+        }
+
+        for (int i = 0; i < inputs.Count; i++)
+        {
+            // for each input, check if its corresponding binary digit is 1 on the cyclestate
+            inputs[i].SetState((TwoToThe(i) & _cycleState) > 0);
+            inputs[i].UpdateConnected();
+        }
+        
+    }
+    private void Update()
+    {
+        _textTimeout -= Time.deltaTime;
+        if (_textTimeout <= 0)
+        {
+            text.text = "";
+        }
     }
 
     public bool Test()
@@ -38,31 +87,41 @@ public class Level : MonoBehaviour
                 inputs[i].SetState(truthElement.input[i]);
                 text.text += truthElement.input[i] ? "1" : "0";
             }
-            
+
             // update the connections instantly
             for (int i = 0; i < inputs.Count; i++)
             {
                 inputs[i].UpdateConnected(0, 100);
             }
+
             // check if output is correct
             for (int i = 0; i < outputs.Count; i++)
             {
+                bool thisoneiscorrect = truthElement.expectedOutput[i] == outputs[i].on;
                 // // if any mismatch detected, return false
-                if (truthElement.expectedOutput[i] != outputs[i].on)
+                text.text += " | ";
+                if (!thisoneiscorrect)
                 {
                     correct = false;
+                    text.text += "<color=red>";
                 }
 
-                text.text += " | Expected: ";
+                text.text += "Expected: ";
                 foreach (var expectedBit in truthElement.expectedOutput)
                 {
                     text.text += expectedBit ? "1" : "0";
                 }
 
                 text.text += " | Actual: ";
+
                 foreach (var expectedBit in outputs)
                 {
                     text.text += expectedBit.on ? "1" : "0";
+                }
+
+                if (!thisoneiscorrect)
+                {
+                    text.text += "</color>";
                 }
 
                 text.text += "\n";
@@ -71,6 +130,7 @@ public class Level : MonoBehaviour
 
         text.text += correct ? "Level Complete!" : "Uh oh, something's wrong";
         // no mismatches found, must be true.
+        _textTimeout = 5f;
         return correct;
     }
 }
