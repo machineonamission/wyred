702476b781cacc97cce8f411f6579b4ce56714f2
Melody <reticivis@gmail.com>
2023-02-07 13:48:40 -0600

    bg

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -5,23 +5,28 @@ using UnityEngine;
 public class Background : MonoBehaviour
 {
     public float parallax = 0.1f;
-    public Transform camera;
+    public Camera camera;
+    private Transform _cameraTransform;
+    private CinemachineBrain _cameraBrain;
     private Transform _transform;
     private Vector3 _startpos;
 
-    public CinemachineBrain cineBrain;
+    
 
     private void Start()
     {
         _transform = GetComponent<Transform>();
+        _cameraTransform = camera.GetComponent<Transform>();
+        _cameraBrain = camera.GetComponent<CinemachineBrain>();
         _startpos = _transform.position;
     }
 
     private void Update()
     {
-        Vector3 transformpos = camera.position * parallax + _startpos;
+        Vector3 transformpos = _cameraTransform.position * parallax + _startpos;
         transformpos.z = _startpos.z;
         _transform.position = transformpos;
-        cineBrain.ManualUpdate();
+        // need to tell camera when to update otherwise it goes jittery and silly and goofy
+        _cameraBrain.ManualUpdate();
     }
 }
