96eea31b04ee34abecba3b0bb8dee5179494f895
Melody <reticivis@gmail.com>
2023-02-16 17:23:33 -0600

    credits

--- Assets/Scripts/Title.cs
+++ Assets/Scripts/Title.cs
@@ -7,7 +7,13 @@ public class Title : MonoBehaviour
 {
     public Button newGame;
     public Button loadGame;
+    public Button credits;
     public Button crow;
+
+    public GameObject title;
+    public GameObject buttons;
+    public GameObject creditsText;
+    public Button exitCredits;
 
     private void Start()
     {
@@ -18,6 +24,15 @@ public class Title : MonoBehaviour
             loadGame.onClick.AddListener(LoadGame);
             loadGame.interactable = true;
         }
+
+        newGame.onClick.AddListener(NewGame);
+        newGame.interactable = true;
+
+        credits.onClick.AddListener(Credits);
+        credits.interactable = true;
+
+        exitCredits.onClick.AddListener(ExitCredits);
+        exitCredits.interactable = true;
         // crow.onClick.AddListener(Crow);
         // crow.interactable = true;
     }
@@ -30,6 +45,22 @@ public class Title : MonoBehaviour
     public void NewGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void Credits()
+    {
+        title.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        title.transform.localPosition = new Vector3(0, 150, 0);
+        buttons.SetActive(false);
+        creditsText.SetActive(true);
+    }
+
+    public void ExitCredits()
+    {
+        title.transform.localScale = new Vector3(1, 1, 1);
+        title.transform.localPosition = new Vector3(0, 80, 0);
+        buttons.SetActive(true);
+        creditsText.SetActive(false);
     }
 
     public void Crow()
